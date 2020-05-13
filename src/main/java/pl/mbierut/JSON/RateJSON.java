74b1e9f2d885ccee347f9d3903f616dca4d049f2
mbierut <mbierut@gmail.com>
2020-05-13 19:21:25 +0200

    Fixed JSON rading

--- src/main/java/pl/mbierut/JSON/RateJSON.java
+++ src/main/java/pl/mbierut/JSON/RateJSON.java
@@ -2,11 +2,13 @@ package pl.mbierut.JSON;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class RateJSON{
-    public String number;
+    public String no;
     public String effectiveDate;
     public double bid;
     public double ask;
