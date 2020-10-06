3d9430c25d3062111ba1d0be796297df7c186aa7
mbierut <mbierut@gmail.com>
2020-10-06 16:21:17 +0200

    Database integration

--- src/main/java/pl/mbierut/entities/User.java
+++ src/main/java/pl/mbierut/entities/User.java
@@ -1,22 +1,31 @@
-package pl.mbierut.models;
+package pl.mbierut.entities;
 
 import lombok.Getter;
+import pl.mbierut.models.OrderHistory;
 
 import javax.persistence.*;
 
+@Entity
 @Getter
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @Column(name = "username")
     private String username;
 
+    @Column(name = "email")
     private String email;
 
+    @Column(name = "password")
     private String password;
 
+    @Column(name = "wallet")
     private Wallet wallet;
 
+    @Column(name = "order_history")
     private OrderHistory orderHistory;
 
     public User(String username, String email, String password) {
---
+++ src/main/java/pl/mbierut/entities/Wallet.java
@@ -0,0 +1,92 @@
+package pl.mbierut.entities;
+
+import lombok.Getter;
+import lombok.ToString;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import pl.mbierut.exceptions.InsufficientFundsException;
+import pl.mbierut.models.Funds;
+import pl.mbierut.models.enums.BuyOrSell;
+import pl.mbierut.models.enums.Currency;
+
+import javax.persistence.*;
+import java.util.HashMap;
+import java.util.Map;
+
+@Entity
+@Getter
+@ToString
+public class Wallet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+
+    @Column(name = "currencies")
+    private Map<Currency, Double> currencies;
+
+    @Column(name = "logger")
+    private static Logger logger = LoggerFactory.getLogger(Wallet.class);
+
+//    public void fulfillOrder(Order order) throws InsufficientFundsException {
+//        if (order.getBuyOrSell().equals(BuyOrSell.sell)) {
+//            this.sellCurrency(order);
+//        } else if (order.getBuyOrSell().equals(BuyOrSell.buy)) {
+//            this.buyCurrency(order);
+//        }
+//    }
+//
+//    private void sellCurrency(Order order) throws InsufficientFundsException {
+//        double curSubtractedFrom = this.getCurrencies().get(order.getFundsToBuyOrSell().getCurrency());
+//        if (curSubtractedFrom - order.getFundsToBuyOrSell().getAmount() < 0) {
+//            logger.error("Insufficient funds");
+//            throw new InsufficientFundsException("Insufficient funds");
+//        }
+//        this.getCurrencies().merge(order.getOtherCurrencyToSellOrBuy(), order.getFundsValueInOtherCurrency(), Double::sum);
+//        this.getCurrencies().put(order.getFundsToBuyOrSell().getCurrency(), curSubtractedFrom - order.getFundsToBuyOrSell().getAmount());
+//        logger.info("{}{} was sold for {}{}", order.getFundsToBuyOrSell().getAmount(), order.getFundsToBuyOrSell().getCurrency(), order.getFundsValueInOtherCurrency(), order.getOtherCurrencyToSellOrBuy());
+//    }
+//
+//    private void buyCurrency(Order order) throws InsufficientFundsException {
+//        double curSubtractedFrom = this.getCurrencies().get(order.getOtherCurrencyToSellOrBuy());
+//        if (curSubtractedFrom - order.getFundsValueInOtherCurrency() < 0) {
+//            logger.error("Insufficient funds");
+//            throw new InsufficientFundsException("Insufficient funds");
+//        }
+//        this.getCurrencies().merge(order.getFundsToBuyOrSell().getCurrency(), order.getFundsToBuyOrSell().getAmount(), Double::sum);
+//        this.getCurrencies().put(order.getOtherCurrencyToSellOrBuy(), curSubtractedFrom - order.getFundsValueInOtherCurrency());
+//        logger.info("{}{} was bought for {}{}", order.getFundsToBuyOrSell().getAmount(), order.getFundsToBuyOrSell().getCurrency(), order.getFundsValueInOtherCurrency(), order.getOtherCurrencyToSellOrBuy());
+//    }
+//
+//    public void sendMoney(Wallet wallet, Funds funds) throws InsufficientFundsException {
+//        if (wallet.getCurrencies().get(funds.getCurrency()) - funds.getAmount() < 0) {
+//            logger.error("Insufficient funds");
+//            throw new InsufficientFundsException("Insufficient funds");
+//        }
+//        this.getCurrencies().put(funds.getCurrency(), wallet.getCurrencies().get(funds.getCurrency()) - funds.getAmount());
+//        wallet.getCurrencies().merge(funds.getCurrency(), funds.getAmount(), Double::sum);
+//        logger.info("Sent {}{}", funds.getAmount(), funds.getCurrency());
+//    }
+//
+//    public void addFunds(Funds funds) {
+//        this.getCurrencies().merge(funds.getCurrency(), funds.getAmount(), Double::sum);
+//        logger.info("Added {}{} to the wallet", funds.getAmount(), funds.getCurrency());
+//    }
+//
+//    public void withdrawFunds(Funds funds) throws InsufficientFundsException {
+//        if (this.getCurrencies().get(funds.getCurrency()) - funds.getAmount() < 0) {
+//            logger.error("Insufficient funds");
+//            throw new InsufficientFundsException("Insufficient funds");
+//        }
+//        this.getCurrencies().put(funds.getCurrency(), this.getCurrencies().get(funds.getCurrency()) - funds.getAmount());
+//        logger.info("Deposited {}{}", funds.getAmount(), funds.getCurrency());
+//    }
+
+    Wallet() {
+        this.currencies = new HashMap<>();
+        for (Currency currency : Currency.values()) {
+            this.currencies.put(currency, 0.0);
+        }
+    }
+
+}
