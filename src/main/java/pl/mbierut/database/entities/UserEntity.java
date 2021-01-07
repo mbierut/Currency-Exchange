9ce32a50acf96e94a5d8b828cfde90c38fcefe4a
mbierut <mbierut@gmail.com>
2021-01-07 15:12:39 +0100

    Removed rest of old logger implementation

--- src/main/java/pl/mbierut/database/entities/UserEntity.java
+++ src/main/java/pl/mbierut/database/entities/UserEntity.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.transaction.annotation.Transactional;
 import pl.mbierut.exceptions.InsufficientFundsException;
 import pl.mbierut.models.Funds;
 import pl.mbierut.models.enums.BuyOrSell;
@@ -58,12 +57,10 @@ public class UserEntity {
         Funds fundsAddedTo = this.findCurrencyInWallet(order.getOtherCurrencyToSellOrBuy()).getFunds();
         double curSubtractedFrom = fundsSold.getAmount();
         if (curSubtractedFrom - order.getFundsToBuyOrSell().getAmount() < 0.0) {
-//            logger.error("Insufficient funds");
             throw new InsufficientFundsException("Insufficient funds");
         }
         fundsSold.setAmount(curSubtractedFrom - order.getFundsToBuyOrSell().getAmount());
         fundsAddedTo.setAmount(fundsAddedTo.getAmount() + order.getFundsValueInOtherCurrency());
-//        logger.info("{}{} was sold for {}{}", order.getFundsToBuyOrSell().getAmount(), order.getFundsToBuyOrSell().getCurrency(), order.getFundsValueInOtherCurrency(), order.getOtherCurrencyToSellOrBuy());
     }
 
     private void buyCurrency(OrderEntity order) throws InsufficientFundsException {
@@ -71,39 +68,32 @@ public class UserEntity {
         Funds fundsSold = this.findCurrencyInWallet(order.getOtherCurrencyToSellOrBuy()).getFunds();
         double curSubtractedFrom = fundsSold.getAmount();
         if (curSubtractedFrom - order.getFundsValueInOtherCurrency() < 0.0) {
-//            logger.error("Insufficient funds");
             throw new InsufficientFundsException("Insufficient funds");
         }
         fundsSold.setAmount(curSubtractedFrom - order.getFundsValueInOtherCurrency());
         fundsAddedTo.setAmount(fundsAddedTo.getAmount() + order.getFundsToBuyOrSell().getAmount());
-//        logger.info("{}{} was bought for {}{}", order.getFundsToBuyOrSell().getAmount(), order.getFundsToBuyOrSell().getCurrency(), order.getFundsValueInOtherCurrency(), order.getOtherCurrencyToSellOrBuy());
     }
 
     public void sendMoney(UserEntity user, Funds funds) throws InsufficientFundsException {
         Funds baseFunds = this.findCurrencyInWallet(funds.getCurrency()).getFunds();
         if (baseFunds.getAmount() - funds.getAmount() < 0.0) {
-//            logger.error("Insufficient funds");
             throw new InsufficientFundsException("Insufficient funds");
         }
         user.addFunds(funds);
         baseFunds.setAmount(baseFunds.getAmount() - funds.getAmount());
-//        logger.info("Sent {}{}", funds.getAmount(), funds.getCurrency());
     }
 
     public void addFunds(Funds addedFunds) {
         Funds baseFunds = this.findCurrencyInWallet(addedFunds.getCurrency()).getFunds();
         baseFunds.setAmount(baseFunds.getAmount() + addedFunds.getAmount());
-//        logger.info("Added {}{} to the wallet", funds.getAmount(), funds.getCurrency());
     }
 
     public void withdrawFunds(Funds funds) throws InsufficientFundsException {
         Funds baseFunds = this.findCurrencyInWallet(funds.getCurrency()).getFunds();
         if (baseFunds.getAmount() - funds.getAmount() < 0.0) {
-//            logger.error("Insufficient funds");
             throw new InsufficientFundsException("Insufficient funds");
         }
         baseFunds.setAmount(baseFunds.getAmount() - funds.getAmount());
-//        logger.info("Deposited {}{}", funds.getAmount(), funds.getCurrency());
     }
 
     public WalletEntryEntity findCurrencyInWallet(Currency currency) {
